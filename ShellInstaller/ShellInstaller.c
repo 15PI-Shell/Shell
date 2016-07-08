5ad6021234eec9d0d7d28c20401587caf0ec419d
Acuion <natarur@yandex.com>
2016-07-08 15:27:04 +0300

    Базовая логика запуска установщика
    Установка/Удаление в зависимости от наличия записи в реестре, запуск
    сборки установщика при передаче ключа -mkinstaller и пути

--- ShellInstaller/ShellInstaller.c
+++ ShellInstaller/ShellInstaller.c
@@ -1,8 +1,43 @@
+#include <Windows.h>
+#include <string.h>
 
+void MakeInstaller(char* pathWithFiles)
+{
+	printf("MakeInstaller logic: %s\n", pathWithFiles);
+}
+
+HKEY shellRegKey;
+
+void Install()
+{
+	printf("Installer logic\n");
+}
+
+void Uninstall()
+{
+	printf("Uninstaller logic\n");
+}
 
 int main(int argc, char* argv[])
 {
+	if (1 == argc)
+	{
+		LSTATUS kStatus = RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\15PI-Shell", 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &shellRegKey);
+		if (kStatus == ERROR_ACCESS_DENIED)
+		{
+			printf("Admin access required\n");
+			return 0;
+		}
+		if (kStatus == ERROR_FILE_NOT_FOUND)
+			Install();
+		else if (kStatus == ERROR_SUCCESS)
+			Uninstall();
+		else
+			printf("%d registry error\n", kStatus);
+	}
+	else if (3 == argc && 0 == strcmp(argv[1], "-mkinstaller"))
+		MakeInstaller(argv[2]);
 
-
+	system("pause");//press any key...
 	return 0;
 }
