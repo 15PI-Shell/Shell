5992416d07fe41d6829d6272af13819cf7f8ee98
FionaRo <sergeeva.e.d@yandex.ru>
2016-07-12 02:57:20 +0300

    Добавление/удаление данных в/из реестра
    Имя, Расположения, Путь к иконке, Путь к файлу деинсталлятора, размер,
    имя издателя
    P.S. в прошлый раз изменения почему-то отправились не все

--- ShellInstaller/ShellInstaller.c
+++ ShellInstaller/ShellInstaller.c
@@ -1,6 +1,5 @@
 ﻿#include "ShellInstaller.h"
 
-
 void MakeInstaller(char* pathWithFiles)
 {
 	printf("MakeInstaller logic: %s\n", pathWithFiles);
@@ -8,6 +7,7 @@ void MakeInstaller(char* pathWithFiles)
 
 void Install()
 {
+	char copyInstalPath[MAX_PATH];
 	printf("Installer logic\n");
 	printf("Please, enter installation path: ");
 	scanf("%s", &InstallationPath);
@@ -27,6 +27,31 @@ void Install()
 		return;
 	}
 	RegCloseKey(pathRegKey);
+	//Создание раздела 15PI-SHELL
+	RegCloseKey(shellRegKey);
+	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_CREATE_SUB_KEY | KEY_WOW64_64KEY, &shellRegKey);
+	RegFunStatus = RegCreateKeyA(shellRegKey, "15PI-SHELL", &shellRegKey);
+	if (RegFunStatus == ERROR_SUCCESS)
+		printf("Section registry successfully created\n");
+	else
+	{
+		printf("Creating a Registry Key Failed\n");
+		return;
+	}
+	RegCloseKey(shellRegKey);
+	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\15PI-SHELL", 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &shellRegKey);
+	//добавление DisplayIcon
+	strcpy(copyInstalPath, InstallationPath);
+	strcat(copyInstalPath, "\\15PI-SHELL.exe");
+	//добавление DisplayName
+	//добавление InstallLocation
+	//добавление UninstallString
+	strcpy(copyInstalPath, InstallationPath);
+	strcat(copyInstalPath, "\\Uninstall.exe");
+	//добавление Publisher 
+	//добавление EstimatedSize
+	DWORD size = 0xFFFFFF;
+	printf("The data was successfully written to the registry\n");
 }
 
 void Uninstall()
@@ -36,6 +61,7 @@ void Uninstall()
 	GetCurrentDirectoryA(sizeof(InstallationPath), InstallationPath);
 	//strcpy(InstallationPath, "C:\\Game"); На проверку путь вводить без последнего слеша (GetCurrentDirectory получает такой путь)
 	RegGetValueA(pathRegKey, 0, "PATH", RRF_RT_ANY, 0, BufPath, &SizeBufPath);
+	char CurPath[MAX_PATH], copyPath[10000] = "";
 	int i = -1, j = -1, CheckOnePath = 0;
 	strcat(BufPath, ";");
 	while (BufPath[++i])
@@ -69,6 +95,17 @@ void Uninstall()
 		return;
 	}
 	RegCloseKey(pathRegKey);
+	//удаление раздела из реестра
+	RegCloseKey(shellRegKey);
+	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &shellRegKey);
+	RegFunStatus = RegDeleteTreeA(shellRegKey, "15PI-SHELL");
+	if (RegFunStatus == ERROR_SUCCESS)
+		printf("Data successfully deleted from the registry\n");
+	else
+	{
+		printf("Unknown Error\n");
+		return;
+	}
 }
 
 int main(int argc, char* argv[])
