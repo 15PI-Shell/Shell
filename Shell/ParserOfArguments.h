24075fb728070b6cab047c0c44d5ba9eac8c46ef
Anastasia <stasy.k@mail.ru>
2016-07-11 10:37:23 +0300

    Парсер аргументов

---
+++ Shell/ParserOfArguments.h
@@ -0,0 +1,11 @@
+#ifndef ParserOfArguments
+#define ParserOfArguments
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include "StringList.h"
+
+void ParsOfArgs(char* str);
+
+#endif
