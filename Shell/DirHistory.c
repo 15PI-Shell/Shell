ed989612bc4e271d67855e3828b60e27fd1f0754
FionaRo <sergeeva.e.d@yandex.ru>
2016-07-13 13:15:30 +0300

    Добавлена функция, возвращающая путь к директории с историей
    Будет модифицирована.

---
+++ Shell/DirHistory.c
@@ -0,0 +1,15 @@
+﻿#include "DirHistory.h"
+
+char* GetAndCheckPathHistory()
+{
+	HKEY userRegKey;
+	char user[100];
+	DWORD sizeUser = sizeof(user);
+	char HistoryDir[MAX_PATH] = "C:";
+	RegOpenKeyExA(HKEY_CURRENT_USER, "Volatile Environment", 0, KEY_QUERY_VALUE | KEY_WOW64_64KEY, &userRegKey);
+	RegGetValueA(userRegKey, 0, "HOMEPATH", RRF_RT_ANY, 0, user, &sizeUser);
+	strcat(HistoryDir, user);
+	strcat(HistoryDir, "\\Documents\\15PI-SHELL");
+	CreateDirectoryA(HistoryDir, NULL);
+	return HistoryDir;
+}
---
+++ Shell/DirHistory.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <Windows.h>
+
+//функция создает папку для истории, если она не существует и возвращает путь к этой папке
+char* GetAndCheckPathHistory();
