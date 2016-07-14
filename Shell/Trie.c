4c9302bd54ac969d8ffd111a8c7b508cad81946a
Acuion <natarur@yandex.com>
2016-07-14 23:37:15 +0300

    Исправлен баг в дереве, добавлена поддержка цифр

--- Shell/Trie.c
+++ Shell/Trie.c
@@ -1,10 +1,19 @@
 ﻿#include "Trie.h"
 
+int CharGetIndex(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return tolower(c) - 'a';
+	if (c >= '0' && c <= '9')
+		return 26 + c - '0';
+	return -1;
+}
+
 //инициализирует и возвращает корень префиксного дерева
 TrieNode* Trie_Create()
 {
 	TrieNode* trieRoot = (TrieNode*)malloc(sizeof(TrieNode));
-	for (int i = 0; i < 26; ++i)
+	for (int i = 0; i < 36; ++i)
 		trieRoot->children[i] = 0;
 	trieRoot->hasAValue = 0;
 	trieRoot->key = 0;//инициализируем корень, он не содержит никаких значений. Используется чтобы не создавать по дереву на каждую букву
@@ -14,7 +23,7 @@ TrieNode* Trie_Create()
 //функция, возвращающая узел дерева, которому соотвествует указанная строка
 TrieNode* FindInTrieRec(TrieNode* node, char* name)
 {
-	if (!(tolower(name[0]) >= 'a' && tolower(name[0]) <= 'z'))
+	if (CharGetIndex(name[0]) == -1)
 		return 0;
 
 	if (0 == node)
@@ -24,15 +33,15 @@ TrieNode* FindInTrieRec(TrieNode* node, char* name)
 		return node;//если следующий символ строки - 0, то возвращаем этот узел - работа закончена.
 
 					//name + 1 каждую итерацию как бы откусывает первый символ от строки
-	return FindInTrieRec(node->children[tolower(name[1]) - 'a'], name + 1);//дерево регистронезависимо, поэтому переводим любой символ в нижний регистр
+	return FindInTrieRec(node->children[CharGetIndex(name[1])], name + 1);//дерево регистронезависимо, поэтому переводим любой символ в нижний регистр
 }
 
 //оболочка для запуска рекурсивной функции
 TrieNode* Trie_Find(TrieNode* trieRoot, char* name)
 {
-	if (!(tolower(name[0]) >= 'a' && tolower(name[0]) <= 'z'))
+	if (CharGetIndex(name[0]) == -1)
 		return 0;
-	return FindInTrieRec(trieRoot->children[tolower(name[0]) - 'a'], name);
+	return FindInTrieRec(trieRoot->children[CharGetIndex(name[0])], name);
 }
 
 //функция, регистрирующая программу в контроллере
@@ -41,26 +50,31 @@ void Trie_RegisterRec(TrieNode** node, char* name, void* data)
 	if (0 == *node)
 	{//создаЄм новый узел в префиксном дереве, если он ещЄ не существует
 		*node = (TrieNode*)malloc(sizeof(TrieNode));
-		for (int i = 0; i < 26; ++i)
+		for (int i = 0; i < 36; ++i)
 			(*node)->children[i] = 0;//зануляем всех потомков - их ещЄ нет
 		(*node)->hasAValue = 0;//по умолчанию этот узел не содержит описание программы
 		(*node)->key = *name;//подписываем текущий узел
 	}
 
-	if (0 == *(name + 1))
+	if (0 == name[1])
 	{//если следующий символ строки 0 - мы на месте
 		(*node)->hasAValue = 1;
 		(*node)->data = data;//заполняем поля и отмечаем узел, как содержащий программу
 		return;
 	}
 
-	Trie_RegisterRec(&(*node)->children[tolower(name[1]) - 'a'], name + 1, data);
+	if (CharGetIndex(name[1]) == -1)
+		return;
+
+	Trie_RegisterRec(&(*node)->children[CharGetIndex(name[1])], name + 1, data);
 }
 
 //оболочка для запуска рекурсивной функции
 void Trie_Register(TrieNode* trieRoot, char* name, void* data)
 {
-	Trie_RegisterRec(&trieRoot->children[tolower(name[0]) - 'a'], name, data);
+	if (CharGetIndex(name[0]) == -1)
+		return;
+	Trie_RegisterRec(&trieRoot->children[CharGetIndex(name[0])], name, data);
 }
 
 //функция, реализующая обход в глубину и сохраняющая все найденные названия программ в список
@@ -77,6 +91,6 @@ void Trie_FindAllValuesInSubtree(TrieNode* node, char* prefix, int prefixLen, Si
 		SingleLinklistAddDownmost(values, prefix, strlen(prefix));//помещаем в конец списка
 	}
 
-	for (int i = 0; i < 26; ++i)
+	for (int i = 0; i < 36; ++i)
 		Trie_FindAllValuesInSubtree(node->children[i], prefix, prefixLen + 1, values);//продолжаем обход по всем возможным потомкам
 }
