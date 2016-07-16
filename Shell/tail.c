c1de4a609f723f2d1a8515249a640fe2734d02f9
katherina <aniretake.avodia@yandex.ru>
2016-07-17 00:48:35 +0300

    Изменена функция tail с учетом возврата парсера аргументов

--- Shell/tail.c
+++ Shell/tail.c
@@ -5,53 +5,51 @@ char* TailOfFile(char *args)
 	SingleLinklistNode *ArgumentsList = NULL; // создаем список дл€ парсинга аргументов
 	char *fileName = (char*)malloc(MAX_PATH + 1);// строка под им€ файла
 	int n = 0;// счетчик считываемых строк
+	int n_args;
 	FILE *fp = NULL;
-	ParsOfArgs(args, &ArgumentsList);
-	if (ArgumentsList)// если есть аргумент, то это число
+	n_args = ParsOfArgs(args, &ArgumentsList);
+	if (n_args != 2)
 	{
-		sscanf((char*)ArgumentsList->value, "%d", n);//определ€ем кол-во строк
-		if (n <= 0)// что-то не так с аргументом
-		{
-			printf("Incorrect argument\n");
-			return -1;
-		}
-		if (ArgumentsList->up)// если есть еще один аргумент выше
-		{
-			ArgumentsList = ArgumentsList->up;
-			strcpy(fileName, (char*)ArgumentsList->value);//читаем им€ файла
-			fp = fopen(fileName, "r");
-			if (fp == 0) {
-				printf("File reading Error\n");//если не удлось открыть или файла просто нет
-				return -1;
-			}
-			fseek(fp, 0, SEEK_END);
-			long pos = ftell(fp);// ищем конец файла
-			if (pos > 0)// провер€ем на пустоту
-			{
-				rewind(fp);
-				char **mas = (char **)calloc(n, sizeof(char *));//выдел€ем пам€то под массив
-				int size = 0;
-				while (!feof(fp))
-				{
-					fgets(mas[size% n], 255, fp);
-					size++;
-				}
-				for (int i = 0; i < n; i++)
-				{
-					printf("%s\n", mas[i]);
-				}
-				return 0;
-			}
-			else
-			{
-				printf("%s file is empty\n", fileName);
-				return 0;
-			}
-			close(fp);
-			free(fileName);
-		}
+		printf("list of arguments is wrong\n");
+		return -1;
 	}
-		printf("The number of arguments is not enough\n");
-			return  -1;
-	
+	sscanf((char*)ArgumentsList->value, "%d", n);//определ€ем кол-во строк
+	if (n <= 0)// что-то не так с аргументом
+	{
+		printf("Incorrect argument\n");
+		return -1;
+	}
+	ArgumentsList = ArgumentsList->up;
+	strcpy(fileName, (char*)ArgumentsList->value);//читаем им€ файла
+	fp = fopen(fileName, "r");
+	if (fp == 0) {
+		printf("File reading Error\n");//если не удлось открыть или файла просто нет
+		return -1;
+	}
+	fseek(fp, 0, SEEK_END);
+	long pos = ftell(fp);// ищем конец файла
+	if (pos > 0)// провер€ем на пустоту
+	{
+		rewind(fp);
+		char **mas = (char **)calloc(n, sizeof(char *));//выдел€ем пам€то под массив
+		int size = 0;
+		while (!feof(fp))
+		{
+			fgets(mas[size% n], 255, fp);
+			size++;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			printf("%s\n", *mas[i]);
+		}
+		free(mas);
+		return 0;
+	}
+	else
+	{
+		printf("%s file is empty\n", fileName);
+		return 0;
+	}
+	close(fp);
+	free(fileName);
 }
